1ff8333e7737d4a02ebc5fb0f67892a911efde3b
aOpenbrier <aopenbrier@gmail.com>
2018-08-23 16:35:56 -0700

    attack button gameplay logic

--- assets/javascipt/game.js
+++ assets/javascipt/game.js
@@ -1,41 +1,29 @@
-/*
-global variables
-    Array of character-objects
-        hP num
-        attack num
-        counter num
-        active boolean
-    fighter selected index-num
-    fighter current attack power num
-    opponent selected index-num
-    is selection enabled
-*/
-let charArr = [
+const charArr = [
     {
         name: 'Mel C',
-        hp: 100,
+        hp: 145,
         attack: 6,
         counter: 20,
         img: 'melc.jpg'
     },
     {
         name: 'Emma',
-        hp: 100,
+        hp: 130,
         attack: 8,
         counter: 10,
         img: 'emma.jpg'
     },
     {
         name: 'Victoria',
-        hp: 90,
-        attack: 8,
-        counter: 15,
+        hp: 130,
+        attack: 9,
+        counter: 12,
         img: 'victoria.jpg'
 
     },
     {
         name: 'Mel B',
-        hp: 120,
+        hp: 135,
         attack: 7,
         counter: 25,
         img: 'melb.jpg'
@@ -56,21 +44,21 @@ function newGame() {   //new gameinitialize game with characters in selection ar
     ftrIndex = -1
     defIndex = -1
     activeArr = [0, 1, 2, 3]
-    selectable()
+    selectable('first')
     $('#fighter').empty()
     $('#defender').empty()
     $('#alert').html(`<h4>Select your fighter</h4>`)
     $('.vs').css('display', 'none')
-    $('#reset').css('display', 'none')
     $('#attack').css('visibility', 'hidden')
+    $('#reset').css('display', 'none')
 }
 
 //display selectable spices
-function selectable() {
+function selectable(styleClass) {
     $('#selection').empty()
     activeArr.forEach(function (i) {
         $('#selection').append(`
-            <div class="charbox choice" value="${i}">
+            <div class="charbox choice ${styleClass}" value="${i}">
             <img class="photo" src="assets/images/${charArr[i].img}" alt="${charArr[i].name}">
             <h3 class="name">${charArr[i].name}</h3>
             <h3 class="hp">HP: ${charArr[i].hp}</h3>
@@ -84,12 +72,12 @@ $(document).on('click', '.choice', function () { //on click spice selection
         ftrIndex = $(this).attr('value') - 0
         ftrHP = charArr[ftrIndex].hp
         ftrAP = charArr[ftrIndex].attack
-        activeArr.splice(ftrIndex, 1) //splice spice from selectable
+        activeArr.splice(ftrIndex, 1) //splice spice from selectable 
         $('#fighter').html(`<img class="photo" src="assets/images/${charArr[ftrIndex].img}" alt="${charArr[ftrIndex].name}">
             <h3 class="name">${charArr[ftrIndex].name}</h3>
             <h3 class="hp" id="ftrHP">HP: ${ftrHP}</h3>`)
         $('#alert').html(`<h4>Select your opponent</h4>`)
-        selectable()
+        selectable('second')
         $('.vs').css('display', 'flex')
         $('#reset').css('display', 'inline')
     }
@@ -101,29 +89,53 @@ $(document).on('click', '.choice', function () { //on click spice selection
         $('#defender').html(`<img class="photo" src="assets/images/${charArr[defIndex].img}" alt="${charArr[defIndex].name}">
             <h3 class="name">${charArr[defIndex].name}</h3>
             <h3 class="hp" id="defHP">HP: ${defHP}</h3>`)
-            $('#selection').empty()
-            $('#attack').css('visibility', 'visible')
-            $('#alert').html(`<h4>Try attacking</h4>`)
+        $('#selection').empty()
+        $('.vs').css('display', 'flex')
+        $('#attack').css('visibility', 'visible')
+        $('#alert').html(`<h4>Try attacking</h4>`)
     }
 })
 
 // Attack button function
 $('#attack').on('click', function () {
-    // defender hp -= fighter current attack power
-    // message you inflicted ## damage
-    // if def hp < 1 
-        // win fight
-        // disable attack button
-        // if no defenders
-            // win game
-        // else
-            // "choose a defender" message
-            // defender selection enabled
-    // else
-        // fighter current attack += base attack power
-        // fighter hp -= defender object.counter
-        // message defender inflicted ## damage
-        // if fighter hp <1 lose
+    if (defIndex > -1) {
+        defHP -= ftrAP
+        $('#alert').html(`<h4>You inflicted ${ftrAP} damage</h4>`)
+        if (defHP < 1) { //Defender HP drops to 0, win round
+            $('#alert').html(`<h4>You defeated ${charArr[defIndex].name}!</h4>`)
+            $('#defender').empty()
+            $('.vs').css('display', 'none')
+            $('#attack').css('visibility', 'hidden')
+            if (activeArr.length === 0) { //if no opponents remain, win game
+                $('#alert').append(`<h4>You are the Spice Supreme!</h4>`)
+
+            }
+            else { //choose next opponent
+                $('#alert').append(`<h4>Select your opponent</h4>`)
+                defIndex = -1
+                selectable('second')
+            }
+        }
+        else {
+            $('#defHP').text(`HP: ${defHP}`)
+            ftrAP += charArr[ftrIndex].attack //increase attack power
+            ftrHP -= charArr[defIndex].counter //defender count attacks
+            $('#alert').append(`
+                <h4>${charArr[defIndex].name} counter-attacked. You received ${charArr[defIndex].counter} damage</h4>
+                `)
+            if (ftrHP < 1) { //Fighter HP drops to 0, Lose game
+                $('.vs').css('display', 'none')
+                $('#attack').css('visibility', 'hidden')
+                $('#alert').html(`
+                    <h4>You were defeated by ${charArr[defIndex].name}!</h4>
+                    <h4>Press Reset to play again.</h4>
+                     `)
+            }
+            else {
+                $('#ftrHP').text(`HP: ${ftrHP}`)
+            }
+        }
+    }
 })
 
 $('#reset').on('click', function () {
